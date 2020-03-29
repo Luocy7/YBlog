ea9433463e3f7ede7ad95e09461c7f6d6942d3ca
Luocy <luocy77@gmail.com>
2020-03-29 17:39:40 +0800

    fix app.run

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,7 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-
 app = create_app('prd')
+
+if __name__ == '__main__':
+    app.run(port=5001)
