61714ecd29c994c989878cac921c532d62be669d
Luocy <luocy77@gmail.com>
2020-03-29 18:50:49 +0800

    fix dotenv bug

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,11 @@
 import os
 from dotenv import load_dotenv
-from yblog import create_app
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
+
+from yblog import create_app
 
 app = create_app('prd')
 
--- yblog/__init__.py
+++ yblog/__init__.py
@@ -101,7 +101,6 @@ def register_logging(app):
 
 def register_extensions(app):
     # mail.init_app(app)
-    # moment.init_app(app)
     # toolbar.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
