324fa974a1453bce89dfb98a69c50227a44f4c34
Luocy <luocy77@gmail.com>
2020-03-29 20:03:07 +0800

    prd0.2

---
+++ gunicorn_cfg.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 _*-
+"""
+    @author: Luocy
+    @time: 2020/03/29
+    @copyright: © 2020 Luocy <luocy77@gmail.com>
+"""
+
+import gevent.monkey
+import multiprocessing
+
+gevent.monkey.patch_all()
+
+# debug = True
+loglevel = 'info'
+bind = "0.0.0.0:8080"
+pidfile = "log/gunicorn.pid"
+accesslog = "log/access.log"
+errorlog = "log/debug.log"
+# daemon = True
+
+# 启动的进程数
+workers = multiprocessing.cpu_count()
+worker_class = 'gevent'
+x_forwarded_for_header = 'X-FORWARDED-FOR'
